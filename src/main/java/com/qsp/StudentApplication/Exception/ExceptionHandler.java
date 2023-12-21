006bb315e9a3c81438b8d420efed29b4460ec507
arsAravind <saravindan7871@gmail.com>
2023-12-21 11:13:11 +0530

    Commiting branchA

--- src/main/java/com/qsp/StudentApplication/Exception/ExceptionHandler.java
+++ src/main/java/com/qsp/StudentApplication/Exception/ExceptionHandler.java
@@ -7,9 +7,10 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ExceptionHandler {
 @org.springframework.web.bind.annotation.ExceptionHandler(StudentNotFoundException.class)
-	public ResponseEntity<ErrorMessage> notfound(StudentNotFoundException e)
+	public ResponseEntity<Object> notfound(StudentNotFoundException e)
 	{
-		ErrorMessage errorMessage=new ErrorMessage(HttpStatus.BAD_GATEWAY,e.getMessage());
-		return new ResponseEntity<ErrorMessage>(errorMessage,HttpStatus.BAD_GATEWAY);
+		
+		return new ResponseEntity<>(e.getMessage(),HttpStatus.BAD_GATEWAY);
+	
 	}
 }
