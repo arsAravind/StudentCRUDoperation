6632bf2a99a20c139d083a44be7bf823bf6f4a1c
arsAravind <saravindan7871@gmail.com>
2023-12-21 13:31:16 +0530

    vbb

---
+++ src/main/java/com/qsp/StudentApplication/bou.java
@@ -0,0 +1,9 @@
+package com.qsp.StudentApplication;
+
+import org.springframework.boot.SpringApplication;
+
+public class bou {
+public static void main(String[] args) {
+	System.out.println("assdad");
+}
+}
